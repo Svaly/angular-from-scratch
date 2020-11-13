653b0ee49dee52e4242298c6677b0377edfc73f4
Rafał Grzesik <rafal.grzesik@ing.com>
2020-11-13 14:54:26 +0100

    ex3

--- src/app/features/jokes/jokes.component.ts
+++ src/app/features/jokes/jokes.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class JokesComponent {
 
   public displayedJoke: string;
+  private newJoke: string;
   private jokes: string[];
 
   constructor() {
@@ -16,18 +17,28 @@ export class JokesComponent {
       'Joke2',
       'Joke3',
     ];
+
+    this.newJoke = '';
   }
 
   public get jokesList(): string[] {
     return this.jokes;
   }
 
-  public getRandomJoke(): string {
-    return this.jokes[this.randomIntFromInterval(0, this.jokes.length)];
+  public newJokeValueChanged(value: string): void {
+    this.newJoke = value;
+  }
+
+  public addJoke(){
+    this.jokes.push(this.newJoke);
   }
 
   public randomizeJoke(){
     this.displayedJoke = this.getRandomJoke();
+  }
+
+  public getRandomJoke(): string {
+    return this.jokes[this.randomIntFromInterval(0, this.jokes.length)];
   }
 
   private randomIntFromInterval(min: number, max: number): number {
